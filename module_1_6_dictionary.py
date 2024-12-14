9ef23f1a22af7ddc7a0800f0bd62662ffa25dfe6
hishnik777 <klatvitas@gmail.com>
2024-12-15 03:17:01 +0500

    Update module_1_6_dictionary.py

--- module_1_6_dictionary.py
+++ module_1_6_dictionary.py
@@ -1,9 +1,15 @@
-my_dict = {'Vitaliy' : 35, 'Julia' : 42, 'Ilya' : 19}
-print (my_dict)
-print(my_dict['Vitaliy'])
-my_dict['Polina'] = 18
-print (my_dict)
-my_dict.update({'Roma' : 10, 'Vera' : 15})
-my_dict.pop('Vera')
-print (my_dict.get('Roma'))
-print (my_dict)
+My_dict = {'Vitaliy' : 35, 'Julia' : 42, 'Ilya' : 19}
+print ('My_dict:', My_dict)
+
+print('Existing value:', My_dict['Vitaliy'])
+
+# My_dict['Polina'] = 18
+# print (My_dict)
+print('Not existing value:', My_dict.get('Polina'))
+
+My_dict.update({'Roma' : 10, 'Vera' : 15})
+
+a = My_dict.pop('Julia')
+print ('Deleted value:', a)
+
+print (My_dict)

15e9ed5f18de679097b8710d4e8b1aa034a13828
hishnik777 <klatvitas@gmail.com>
2024-12-15 04:22:40 +0500

    Update module_1_6_dictionary.py

--- module_1_6_dictionary.py
+++ module_1_6_dictionary.py
@@ -1,3 +1,6 @@
+
+# Работа со словарями:
+
 my_dict = {'Vitaliy' : 35, 'Julia' : 42, 'Ilya' : 19}
 print ('Dict:', my_dict)
 
@@ -13,3 +16,15 @@ a = my_dict.pop('Julia')
 print ('Deleted value:', a)
 
 print ('Modified dictionary:', my_dict)
+
+
+# Работа с множествами:
+
+my_set = {1, 2, 3, 4, 5, 1, 2, 3, True, False, 'Апельсин', 'Банан', 'Клубника', 3.14, (4, 6, 7), 'Банан', 3.14, True}
+print('Set:', my_set)
+
+my_set.update(["754", "Ананас"])
+
+my_set.discard('Банан')
+
+print('Modified set:', my_set)

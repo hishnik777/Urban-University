aae6d788a6ffb8b2340ef5a271d41eb7eb690c07
hishnik777 <klatvitas@gmail.com>
2024-12-15 02:03:47 +0500

    Add files via upload

---
+++ module_1_6_dictionary.py
@@ -0,0 +1,9 @@
+my_dict = {'Vitaliy' : 35, 'Julia' : 42, 'Ilya' : 19}
+print (my_dict)
+print(my_dict['Vitaliy'])
+my_dict['Polina'] = 18
+print (my_dict)
+my_dict.update({'Roma' : 10, 'Vera' : 15})
+my_dict.pop('Vera')
+print (my_dict.get('Roma'))
+print (my_dict)

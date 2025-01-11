a81830a89881614b9cab0253081c555e92b8ec5c
hishnik777 <klatvitas@gmail.com>
2025-01-11 02:37:06 +0000

    Pending changes exported from your codespace

--- module_2_4_for.py
+++ module_2_4_for.py
@@ -24,5 +24,13 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]  # исходный список
 primes = []  # список, содержащий только простые числа.
 not_primes = []  # список, содержащий все непростые числа.
+
 for i in numbers:
-	print(i)
+	is_prime = True
+#	print(i)
+	
+	for j in range(2, i-1):
+		is_prime != i % j
+	primes.append(i)	
+	print('primes:', primes)
+	print('not_primes:', not_primes)
